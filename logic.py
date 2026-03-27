572848f5a548a8717dd42c1965d68fd557a099f5
conduitpy <conduitpy@gmail.com>
2026-03-27 13:15:17 +0530

    Update logic.py

--- logic.py
+++ logic.py
@@ -1,25 +1,21 @@
 import pandas as pd
 
 def determine_vehicle(df, num_persons):
-    
 
     if num_persons == 0:
         return {
             "found": True,             
             "input_a": 0,              
-            "match_b": 0,              
-           
+            "match_b": 0,                
             "output_c": df.iloc[0]['Assigned_Vehicle'] 
         }
         
-  
     suitable_rows = df[df['Max_Capacity'] >= num_persons]
     
     if not suitable_rows.empty:
-
         match_b = suitable_rows.iloc[0]['Max_Capacity']
         output_c = suitable_rows.iloc[0]['Assigned_Vehicle']
-        
+ 
         return {
             "found": True,
             "input_a": num_persons,
@@ -27,7 +23,6 @@ def determine_vehicle(df, num_persons):
             "output_c": output_c
         }
         
-
     else:
         return {
             "found": False,
